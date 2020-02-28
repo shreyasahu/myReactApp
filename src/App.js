ae104513e5ccfbe7b851335bea212b53d1a4e690
unknown <comviva@MCKD-TRNG30-07.COMVIVA.COM>
2020-02-28 13:02:40 +0530

    successful

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+class App extends Component {
 
-function App() {
+  render (){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet />
+      <Welcome />
     </div>
+  
   );
+}
 }
 
 export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// function Greet(){
+//     return(<h1>Hello Vishwas</h1>);
+// }
+
+const Greet = () => 
+    <h1> Hello Vishwas</h1>
+
+export default Greet 
+
+ 
