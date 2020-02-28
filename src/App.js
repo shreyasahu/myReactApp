cf95f14228ddb9678fb54af75a71e73b06079e3d
unknown <comviva@MCKD-TRNG30-07.COMVIVA.COM>
2020-02-28 15:50:37 +0530

    successful

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+
 class App extends Component {
 
   render (){
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      <Greet name= "Bruce"  heroName="Batman">
+        <p> This is children props</p></Greet>
+      <Greet name="clark" heroName="superman">
+      <button>action</button>
+      </Greet>
+      
+      <Welcome name= "Bruce" heroName="Batman"/>
+      <Welcome name= "Clark" heroName="Superman"/>
+     
+       
+      {/* <Greet />
+      <Welcome /> */}
+      <Hello />
     </div>
   
   );
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>
-                Hello Vishwas 
-            </h1>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h1>
+    //             Hello Vishwas 
+    //         </h1>
+    //     </div>
+    // )
+
+    return React.createElement('div',
+    {id:'hello',className:'dummyClass'},
+    React.createElement('h1',null,'hello shreya'))
 }
 
 export default Hello 
